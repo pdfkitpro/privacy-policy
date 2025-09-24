44f6aa2d662f04bb9dcbcad181a423d1be596290
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-09-24 20:52:05 +0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// This file is currently empty.
+// You can add JavaScript here to enhance the main page.
+// For example, you could add code to filter tool cards based on a search query.
+
+// Tool-specific logic will be in separate files (e.g., merge.js, split.js).
